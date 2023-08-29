32a25687b303bc4a65670c9a56c41f1f07469ac9
colindelotavo <colin.delotavo@gmail.com>
2023-08-29 14:24:24 +1000

    Update on_message()

--- src/reset_names.py
+++ src/reset_names.py
@@ -23,18 +23,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!reset'):
+    # if message.content.startswith('!reset'):
+    if message.content == '!reset':
         for member in message.guild.members:
             try:
-                print(member.display_name)
+                # print(member.display_name)
                 # Reset nickname to default
                 await member.edit(nick=None)
             except Exception as e:
                 print(f"An error occurred for user '{member.display_name}'.\n{e}")
-        await message.channel.send('Nicknames reset!')
+        await message.channel.send('Resetting nicknames!')
+        await message.channel.send('Before/After Placeholder Here', silent=True)
+        await message.channel.send('Before/After Placeholder Here', silent=True)
+        await message.channel.send('Before/After Placeholder Here', silent=True)
 
 client.run(token)
 
 # ideas/notes
 # bot perms dont change admin nickname..?
 # show before and after of name change later
+# if there are 100+ members in a guild, send before and after name change 1 by 1
+# use @silent so that it doesnt spam members
